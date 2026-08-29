a6866f0cbfb9fd40f7b2a022ffcfc3cdfed5ef12
29711575 <29711575@qq.com>
2026-08-29 21:48:37 +0800

    fix: support legacy HPC git in validation runner

--- reproduction/pure_projector_kitaev/run_validation.sh
+++ reproduction/pure_projector_kitaev/run_validation.sh
@@ -7,7 +7,7 @@ threads=${PURE_PROJECTOR_PHASE2_THREADS:-8}
 binary="$here/build/phase2_validation"
 test -x "$binary"
 mkdir -p "$output"
-source_commit=$(git -C "$root" rev-parse HEAD)
+source_commit=$(cd "$root" && git rev-parse HEAD)
 executable_sha=$(sha256sum "$binary" | awk '{print $1}')
 OMP_NUM_THREADS="$threads" MKL_NUM_THREADS=1 \
   "$binary" "$output" "$source_commit" "$executable_sha"
